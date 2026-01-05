8eae2dfecb581e1ba0df850061a8508cd0793acc
aBlackPigeon <ablackpigeon7@gmail.com>
2026-01-05 20:24:57 +0530

    rate limiter setup

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,20 @@ import express from 'express';
 import { connectDb } from './db/connectDB.js';
 import dotenv from 'dotenv';
 import notesRoute from './routes/notes.routes.js';
+import rateLimiter from './middleware/rateLimiter.js';
 
 dotenv.config();
 const PORT = 5001
 const app = express();
 
 app.use(express.json());
+app.use(rateLimiter);
 app.use('/api/notes', notesRoute);
 
-app.listen(PORT , () => {
-    console.log("Server started at PORT" , PORT);
-    connectDb();
-})
+connectDb().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server started at PORT" , PORT);
+    });
+});
+
+
