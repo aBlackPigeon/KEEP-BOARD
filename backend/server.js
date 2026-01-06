a9331285362436b3df17b18be5845e0e4cfb5f04
aBlackPigeon <ablackpigeon7@gmail.com>
2026-01-06 12:33:46 +0530

    fix** production

--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,28 @@ import dotenv from 'dotenv';
 import notesRoute from './routes/notes.routes.js';
 import rateLimiter from './middleware/rateLimiter.js';
 import cors from 'cors';
+import path from 'path'
 
 dotenv.config();
 const PORT = 5001
 const app = express();
+const __dirname = path.resolve();
 
 app.use(cors());
 app.use(express.json());
 app.use(rateLimiter);
 
 
+
 app.use('/api/notes', notesRoute);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+app.get("*" , (req,res) => {
+    res.sendFile(path.join(__dirname,"../frontend", "dist", "index.html"));
+});
+}
 
 connectDb().then(() => {
     app.listen(PORT, () => {
